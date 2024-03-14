d2eeaa45a3b1c921eccf13ee4079f0aa5dac158e
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-03-14 11:47:05 +0100

    Update method is done; created custom editor for the softbody options

--- Assets/Scripts/SoftbodyGenerator.cs
+++ Assets/Scripts/SoftbodyGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using GK;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class SoftbodyGenerator : MonoBehaviour
@@ -368,6 +369,59 @@ public class SoftbodyGenerator : MonoBehaviour
     
     private void Update()
     {
-        throw new NotImplementedException();
+        if (debugMode)
+        {
+            foreach (var jointIndex in noDupesListOfSprings)
+            {
+                Debug.DrawLine(
+                    physicedVertexes[jointIndex.x].transform.position,
+                    physicedVertexes[jointIndex.y].transform.position,
+                    physicedVertexes[jointIndex.x].GetComponent<DebugColorGameObject>().Color);
+            }
+
+            foreach (var jointIndex in noDupesListOfSprings)
+            {
+                Debug.DrawLine(
+                    physicedVertexes[jointIndex.x].transform.position,
+                    centerOfMasObj.transform.position,
+                    Color.red);
+            }
+        }
+
+        var tempVertexes = new Vector3[originalMeshFilter.mesh.vertices.Length];
+        for (int i = 0; i < tempVertexes.Length; ++i)
+        {
+            tempVertexes[i] = physicedVertexes[vertexDictionary[i]].transform.localPosition;
+        }
+        
+        originalMeshFilter.mesh.vertices = tempVertexes;
+        originalMeshFilter.mesh.RecalculateBounds();
+        originalMeshFilter.mesh.RecalculateNormals();
+    }
+}
+
+public class DebugColorGameObject : MonoBehaviour
+{
+    public Color Color { get; set; }
+}
+
+[CustomEditor(typeof(SoftbodyGenerator))]
+public class LookAtPointEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SoftbodyGenerator softbody = target as SoftbodyGenerator;       
+        
+        softbody.debugMode = EditorGUILayout.Toggle("#Debug mod", softbody.debugMode);
+        EditorGUILayout.Space();
+
+        string[] options = new string[] { "  version 1", "  version 2" };
+
+        softbody.gravity = EditorGUILayout.Toggle("Gravity", softbody.gravity);
+        softbody.mass = EditorGUILayout.FloatField("Mass(KG)", softbody.mass);
+        softbody.physicsRoughness = EditorGUILayout.FloatField("Drag (roughness)", softbody.physicsRoughness);
+        softbody.softness = EditorGUILayout.FloatField("Softbody hardness", softbody.softness);
+        softbody.damp = EditorGUILayout.FloatField("Softbody damper", softbody.damp);
+        softbody.collisionSurfaceOffset = EditorGUILayout.FloatField("Softbody Offset", softbody.collisionSurfaceOffset);
     }
 }
